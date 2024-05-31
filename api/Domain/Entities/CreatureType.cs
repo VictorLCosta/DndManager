8757456ea2ad2ac71dd95bed9abe31950624ad7e
VictorLCosta <victorlc2019@outlook.com>
2024-05-31 00:00:37 -0300

    refactoring the whole application to Avalonia UI

--- api/Domain/Entities/CreatureType.cs
+++
@@ -1,8 +0,0 @@
-namespace Domain.Entities
-{
-    public class CreatureType : BaseEntity
-    {
-        public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; }        
-    }
-}
--- api/Domain/Entities/PlayableClass.cs
+++
@@ -1,7 +0,0 @@
-namespace Domain.Entities
-{
-    public class PlayableClass : BaseEntity
-    {
-        public string Name { get; set; } = null!;
-    }
-}
